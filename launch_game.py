b799cf93caae45a19fa7940a8a66346128bfdc03
TheMB04 <122024164+TheMB04@users.noreply.github.com>
2023-05-20 01:20:27 +0200

    Update launch_game.py

---
+++ launch_game.py
@@ -0,0 +1,32 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
+    
+    screen.fill("darkgreen")
+
+    
+
+
+
+
+
+    
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
